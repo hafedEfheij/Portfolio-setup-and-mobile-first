d6a0a4645dd3366f2dca19ca7006a2a72befd5b0
hafed <hafedrs@gmail.com>
2023-03-19 00:19:10 +0200

    fix local storage eslint

--- javascript/localStorage.js
+++ javascript/localStorage.js
@@ -1,5 +1,5 @@
 // This code sets up a form on a webpage and allows users to store their data in local storage
-const contactForm = document.getElementById("contact-form");
+const contactForm = document.getElementById('contact-form');
 const {
   name: nameInput,
   email: emailInput,
@@ -13,19 +13,19 @@ function storageAvailable(type) {
   let storage;
   try {
     storage = window[type];
-    const x = "storage_test";
+    const x = 'storage_test';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
     return (
-      e instanceof DOMException &&
-      (e.code === 22 ||
-        e.code === 1014 ||
-        e.name === "QuotaExceededError" ||
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-      storage &&
-      storage.length !== 0
+      e instanceof DOMException
+      && (e.code === 22
+        || e.code === 1014
+        || e.name === 'QuotaExceededError'
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      && storage
+      && storage.length !== 0
     );
   }
 }
@@ -33,7 +33,7 @@ function storageAvailable(type) {
 // This variable checks whether local storage is available and assigns it to availableStorage
 let availableStorage;
 
-if (storageAvailable("localStorage")) {
+if (storageAvailable('localStorage')) {
   // If local storage is available, use it
   availableStorage = window.localStorage;
 } else {
@@ -50,31 +50,31 @@ function storeData() {
   formData.email = emailInput.value;
   formData.message = messageInput.value;
   const jsonData = JSON.stringify(formData);
-  availableStorage.setItem("contactFormData", jsonData);
+  availableStorage.setItem('contactFormData', jsonData);
 }
 
 // These event listeners call the storeData() function when the input fields change
-nameInput.addEventListener("change", () => {
+nameInput.addEventListener('change', () => {
   storeData();
 });
 
-emailInput.addEventListener("change", () => {
+emailInput.addEventListener('change', () => {
   storeData();
 });
 
-messageInput.addEventListener("change", () => {
+messageInput.addEventListener('change', () => {
   storeData();
 });
 
 // This function retrieves the form data from local storage and populates the form fields with it
 function retrieveData() {
-  const data = availableStorage.getItem("contactFormData");
+  const data = availableStorage.getItem('contactFormData');
   const parseData = JSON.parse(data);
   if (data?.length > 0) {
     const { name, email, message } = parseData;
-    nameInput.value = name || "";
-    emailInput.value = email || "";
-    messageInput.value = message || "";
+    nameInput.value = name || '';
+    emailInput.value = email || '';
+    messageInput.value = message || '';
   }
 }
 
@@ -84,9 +84,9 @@ window.onload = () => {
 };
 
 // This event listener resets the form and clears the local storage when the reset button is clicked
-const btnReset = document.getElementById("btn-reset");
+const btnReset = document.getElementById('btn-reset');
 
-btnReset.addEventListener("click", (event) => {
+btnReset.addEventListener('click', (event) => {
   event.preventDefault();
   contactForm.reset();
   availableStorage.clear();
